bc552fcf2d807faa17d3bda9ef6c737a858b48bf
Pablo Tapia <pablo.tapia@inviu.com.ar>
2023-07-14 11:58:14 -0300

    refactor

---
+++ src/main/java/ar/com/inviu/katas/AccountBalanceService.java
@@ -0,0 +1,16 @@
+package ar.com.inviu.katas;
+
+public class AccountBalanceService {
+
+    public static void executeCashIn(Object amount, Object account){
+        return;
+    }
+
+    public static void executeCashOut(Object amount, Object sourceAccount){
+        return;
+    }
+
+    public static void executeCashTransfer(Object amount, Object sourceAccount, Object destinationAccount){
+        return;
+    }
+}
